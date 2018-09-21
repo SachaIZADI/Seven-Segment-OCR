8643a3fa5376a9b94ece585a780e1120e90e9f57
Alexandre Aria Momeni <aamomeni@bmir-ct-gpu-5.stanford.edu>
2018-09-20 17:39:29 -0700

    end-to-end

--- Datasets.py
+++ Datasets.py
@@ -1,23 +1,21 @@
 import os 
 import pandas as pd 
 import numpy as np 
-import matplotlib.pyplotas plt 
+import matplotlib.pyplot as plt 
 from PIL import Image
 
 class Dataset:
     
-    def __init__(self, config):
-            self.config= config
-            self.csv_directory = self.config.csv_directory
+    def __init__(self):
             self.data = self.full_data()
                         
     def full_data(self):
         suffix = ".csv"
-        csv_directory = self.csv_directory
+        csv_directory = 'Datasets/'
         csv_files = [i for i in os.listdir(csv_directory) if i.endswith( suffix )]
         full_data = []
         for i in range(len(csv_files)):
-            data = pd.read_csv(csv_directory+'/'+csv_files[i], sep=';', index_col = 0)
+            data = pd.read_csv(csv_directory +csv_files[i], sep=';', index_col = 0)
             full_data.append(data)
             
         full_data = pd.concat(full_data, axis=0)
@@ -26,15 +24,15 @@ class Dataset:
         
 class Dataset_Multi(Dataset):
     
-    def __init__(self, config):
-        Dataset.__init__(self, config)
-        self.frame_directory = self.config.frame_directory
+    def __init__(self):
+        Dataset.__init__(self)
+        self.frame_directory = 'Datasets_frames/'
         self.frame_data = self.data[self.data["image"].isin(os.listdir(self.frame_directory))]
         
     def convert_to_arrays(self,samples):
         X = []
         for sample in samples:
-            ID =  'Dataset_frames' + "%s" % (sample)
+            ID =  'Datasets_frames/' + "%s" % (sample)
             img = Image.open(ID)
             img = np.array(img)
             img = img.reshape((img.shape[0],img.shape[1],1))
@@ -45,15 +43,16 @@ class Dataset_Multi(Dataset):
         
 class Dataset_Single(Dataset):
     
-    def __init__(self, config):
-        Dataset.__init__(self, config)
-        self.digits_directory = self.config.digits_directory
+    def __init__(self):
+        Dataset.__init__(self)
+        self.digits_directory = 'Datasets_digits/'
         self.digits_data = self.digits_data()
+
     
-    def data(self):
+    def digits_data(self):
         ids = []
         labels = []
-        for i in range(self.config.n_classes):
+        for i in range(11):
             n_directory = self.digits_directory + '%i' %i
             for j in os.listdir(n_directory):
                 ids.append(directory+j)
@@ -67,7 +66,7 @@ class Dataset_Single(Dataset):
         for sample in samples:
             img = Image.open(sample)
             img = np.array(img)
-            img.resize((self.config.digit_height,self.config.digit_height))
+            img.resize((100,256))
             img = img.reshape((img.shape[0],img.shape[1],1))
             X.append(img)
         X = np.asarray(X)
